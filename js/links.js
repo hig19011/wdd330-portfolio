a46d2ed040a27d9b089e7f33967a40d27b44b728
hig19011 <hig19011@byui.edu>
2021-01-12 23:42:32 -0700

    Week 02 notes and examples

--- js/links.js
+++ js/links.js
@@ -2,5 +2,57 @@ const links = [
   {
     label:"Week 1",
     url: "week01/index.html"
-  }
+  },
+  {
+    label:"Week 2",
+    url: "week02/index.html"
+  },
+  // {
+  //   label:"Week 3",
+  //   url: "week03/index.html"
+  // },
+  // {
+  //   label:"Week 4",
+  //   url: "week04/index.html"
+  // },
+  // {
+  //   label:"Week 5",
+  //   url: "week05/index.html"
+  // },
+  // {
+  //   label:"Week 6",
+  //   url: "week06/index.html"
+  // },
+  // {
+  //   label:"Week 7",
+  //   url: "week07/index.html"
+  // },
+  // {
+  //   label:"Week 8",
+  //   url: "week08/index.html"
+  // },
+  // {
+  //   label:"Week 9",
+  //   url: "week09/index.html"
+  // },
+  // {
+  //   label:"Week 10",
+  //   url: "week10/index.html"
+  // },
+  // {
+  //   label:"Week 11",
+  //   url: "week11/index.html"
+  // },
+  // {
+  //   label:"Week 12",
+  //   url: "week12/index.html"
+  // },
+  // {
+  //   label:"Week 13",
+  //   url: "week13/index.html"
+  // },
+  // {
+  //   label:"Week 14",
+  //   url: "week14/index.html"
+  // }
 ]
