149c0c39bd6ab35f0ecd20bf8dddcb8dae39523d
hig19011 <hig19011@byui.edu>
2021-02-08 22:13:08 -0700

    Added filter code and animations

--- challenge1/js/ToDo.js
+++ challenge1/js/ToDo.js
@@ -7,11 +7,104 @@ class Todos {
     this.storage = new ls();
     let addTask = document.getElementById('addTask');
     addTask.addEventListener('click', () => { this.newTodo() });
+    let filterAll = document.getElementById('filterAll');
+    filterAll.addEventListener('click', () => { this.showAll() });
+    let filterActive = document.getElementById('filterActive');
+    filterActive.addEventListener('click', () => { this.showActive() });
+    let filterCompleted = document.getElementById('filterCompleted');
+    filterCompleted.addEventListener('click', () => { this.showCompleted() });
+  }
+
+  animatedAddToList = (todo, j=-1) => {
+    let newElement = this.createTodoElement(todo);
+    if(j == -1){
+      taskList.appendChild(newElement);
+    }
+    else{
+      taskList.insertBefore(newElement, taskList.children[j]);
+    }
+    newElement.classList.toggle("hidden");
+    setTimeout(() => {
+      newElement.classList.toggle("hidden");
+    }, 1);
+  }
+
+  showAll = () => {
+    let todos = this.storage.getTodoList();
+    todos.sort((a, b)=> { return a.id < b.id ? -1 : a.id > b.id ? 1 : 0 });
+    
+    let taskList = document.getElementById('taskList');    
+    for (let j = 0; j < todos.length; j++){          
+      let todo = todos[j];
+      var child = taskList.children[j];
+      if(child === undefined){
+        this.animatedAddToList(todo);
+        continue;
+      }
+      if (child.attributes['data-id'] == todo.id) {
+        continue;
+      }
+      this.animatedAddToList(todo,j);
+    }      
+  }  
+
+  showActive = () => {
+    let todos = this.storage.getTodoList();
+    todos.sort((a, b)=> { return a.id < b.id ? -1 : a.id > b.id ? 1 : 0 });
+    
+    let taskList = document.getElementById('taskList');
+    for (let j = 0; j < todos.length; j++){          
+      let todo = todos[j];
+      let child = taskList.children[j];
+
+      if(child === undefined && todo.completed == false){
+        this.animatedAddToList(todo);
+        continue;
+      }
+      else if (child.attributes['data-id'] == todo.id && todo.completed == true) {
+        child.classList.toggle("hidden");                   
+        setTimeout(() => {
+          taskList.removeChild(child);
+        }, 333)
+        continue;
+      }
+      else if (child.attributes['data-id'] == todo.id && todo.completed == false) {
+        continue;
+      }
+      this.animatedAddToList(todo,j);
+    }      
+  }
+
+  showCompleted = () => {
+    let todos = this.storage.getTodoList();
+    todos.sort((a, b)=> { return a.id < b.id ? -1 : a.id > b.id ? 1 : 0 });
+    
+    let taskList = document.getElementById('taskList');
+    for (let j = 0; j < todos.length; j++){          
+      let todo = todos[j];
+      let child = taskList.children[j];
+
+      if(child === undefined && todo.completed == true){
+        this.animatedAddToList(todo);
+        continue;
+      }
+      else if (child.attributes['data-id'] == todo.id && todo.completed == false) {
+        child.classList.toggle("hidden");                   
+        setTimeout(() => {
+          taskList.removeChild(child);
+        }, 333)
+        continue;
+      }
+      else if (child.attributes['data-id'] == todo.id && todo.completed == true) {
+        continue;
+      }
+      this.animatedAddToList(todo,j);
+    }         
   }
 
   loadTodos = () => {
     let todos = this.storage.getTodoList();
-    todos.sort((a, b)=> { return a.id < b.id ? -1 : a.id > b.id ? 1 : 0 })
+    todos.sort((a, b)=> { return a.id < b.id ? -1 : a.id > b.id ? 1 : 0 });
     todos.forEach(todo => {
       this.addTodo(todo);
     });
@@ -20,10 +113,11 @@ class Todos {
   addTodo = (todo) => {
     let todoElement = this.createTodoElement(todo);
     let taskList = document.getElementById('taskList');
-    taskList.appendChild(todoElement);
-    setTimeout(function() {
-      todoElement.classList.toggle("show");
-    }, 10);
+    todoElement.classList.toggle("hidden");
+    taskList.appendChild(todoElement);    
+    setTimeout(() => {
+      todoElement.classList.toggle("hidden");
+    }, 1)
   }
 
   newTodo = () => {
@@ -74,8 +168,11 @@ class Todos {
     for (let i = 0; i < taskList.children.length; i++) {
       var child = taskList.children[i];
       if (child.attributes['data-id'] == id) {
-        this.storage.deleteTodo(id);
-        taskList.removeChild(child);
+        child.classList.toggle("hidden");        
+        this.storage.deleteTodo(id);        
+        setTimeout(() => {
+          taskList.removeChild(child);
+        }, 333)
         break;
       }
     }
