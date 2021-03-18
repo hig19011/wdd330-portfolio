a8cc5ea45a6eb86a02a337f3b1c89931c15d410a
hig19011 <hig19011@byui.edu>
2021-03-17 20:19:16 -0700

    moved user setting to workspace, updated main links

--- week11/js/links.js
+++ week11/js/links.js
@@ -1,18 +1,6 @@
 const links = [
   {
-    label:"Notes",
-    url: "notes.html"
-  },  
-  {
-    label:"Examples Client Side Validation",
-    url: "example-client-side-validation.html"
-  },  
-  {
-    label:"Example JavaScript Validation",
-    url: "example-javascript-validation.html"
-  },    
-  {
     label: "Team Activity",
-    url: "team-activity/index.html"
+    url: "team-activity/client/index.html"
   }
 ]
