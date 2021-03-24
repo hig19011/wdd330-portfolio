5154e56b16c678e1dcb537050319b0f87797c04d
hig19011 <hig19011@byui.edu>
2021-03-24 12:55:01 -0700

    added display of details page

--- challenge2/js/Book.js
+++ challenge2/js/Book.js
@@ -3,23 +3,83 @@ export default class Book {
 
   constructor() {
     this.baseSearchUrl = "http://openlibrary.org/search.json?"
-    this.baseBookUrl = "https://openlibrary.org/works/"
+    this.baseBookUrl = "https://openlibrary.org"
 
     this.Title = "Unknown";
     this.ISBN = "Unknown";
     this.Published = "Unknown";
     this.Subject = "Unknown";
     this.Description = "Unknown";
-    this.CoverSmall = "Unknown";
-    this.CoverLarge = "Unknown";
+ 
     this.Series = "Unknown";
     this.Pages = "Unknown";
     this.Languages = "Unknown";
     this.Publishers = "Unknown";
+    this.Key = "";
+    this.EditionKey = "";
 
     this.Cover = "";
+    //this.Covers = [];
 
     this.searchedBooks = [];
+  }
+
+ 
+
+  //  need to use "Works API" to get description and other fields, may need to make additional API calls for author and other data.
+  fillBook = (data, book) => {
+    //console.log(data);
+    if(book == undefined){
+      var book = new Book();
+    }
+
+    if(data.title){
+      book.Title = data.title;
+    }
+
+    if(data.cover_i){
+      book.Cover = data.cover_i;
+    } else if(data.covers){
+      book.Cover = data.covers[0];
+    }
+
+    if(data.isbn) {
+      book.ISBN = data.isbn[0];
+    }
+
+    if(data.author_name){      
+      book.Author = data.author_name[0];
+    }
+
+    if(data.first_publish_year){
+      book.Published = data.first_publish_year; 
+    }
+    else if(book.publish_date){
+      book.Published = data.publish_date[0];
+    }
+   
+    if (data.subjects) {
+      book.Subject = data.subjects.reduce( (accum, curVal, curIdx, array ) =>{return accum+curVal+', '}, "");
+    } else  if (data.subject) {
+      book.Subject = data.subject.reduce( (accum, curVal, curIdx, array ) =>{return accum+curVal+', '}, "");
+    } 
+
+    if(data.key){
+      book.Key = data.key;
+    }
+    if(data.edition_key){
+      book.EditionKey = data.edition_key;
+    }
+
+    if(data.description){
+      if(typeof data.description === "string"){
+        book.Description = data.description;
+      } else if(data.description.value){
+        book.Description = data.description.value;
+      }
+    }
+    
+    return book;
   }
 
   searchForBooks = async (keyWords, subject, title, author, page=1) => {
@@ -54,8 +114,8 @@ export default class Book {
     
     if (searchParams.length > 0) {
       searchParams += "&";
-     }
-     searchParams += "page="+page;
+    }
+    searchParams += "page="+page;
 
     url = url + searchParams;
     this.totalBooksFound = 0;
@@ -72,51 +132,16 @@ export default class Book {
     return { totalBooksFound: this.totalBooksFound, searchedBooks: this.searchedBooks };
   }
 
-  //  need to use "Works API" to get description and other fields, may need to make additional API calls for author and other data.
-  fillBook = (data) => {
-    //console.log(data);
-    var book = new Book();
-    if(data.title){
-      book.Title = data.title;
-    }
-    if(data.cover_i){
-      book.Cover = data.cover_i;
-    }
-
-
-    if(data.isbn) {
-      book.ISBN = data.isbn[0];
-    }
-
-    if(data.author_name){      
-      book.Author = data.author_name[0];
-    }
-
-    if(data.first_publish_year){
-      book.Published = data.first_publish_year; 
-    }
-    else if(data.publish_date){
-      book.Published = data.publish_date[0];
-    }
-   
-    if(data.subject){
-      book.Subject = data.subject.reduce( (accum, curVal, curIdx, array ) =>{return accum+curVal+', '}, "");
-    }   
-    if(data.key){
-      book.Key = data.key;
-    }
-    
-    return book;
-  }
-
-  getBookDetails = async (id) => {
+  getBookDetails = async (book) => {
+    let id = book.edition_key;
+    id = book.Key;
     let url = this.baseBookUrl + id + ".json";
     console.log(url);
     //url = "https://openlibrary.org/works/OL19749687W.json"
     console.log(url);
     this.bookDetails = await fetch(url)
       .then(response => response.json())
-      .then(data => this.fillBook(data))
+      .then(data => this.fillBook(data, book))
       .catch(e => console.log(e));
 
     return this.bookDetails;
